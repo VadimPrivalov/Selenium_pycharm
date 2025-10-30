acbd009f3e32720532cff8e97d72ae8be5b7c9f7
Vadim <privalovvadim43@gmail.com>
2025-10-30 16:51:53 +0500

    New project

---
+++ POM_saucedemo (1)/POM_saucedemo/run_tests.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import questionary
+
+
+def run_pytest(args: list):
+    """Функция для заупска pytest с заданными аргументами"""
+    command = ["pytest"] + args
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError:
+            print("Во время выполнения теста произошла ошибка")
+    except FileNotFoundError: print("Команда pytest не найдена."
+                                    "Убедитесь,что pytest установлен и доступен в PATH")
+
+
+def main_menu():
+    """"Главное меню CLI"""
+    while True:
+        choice = questionary.select("Добро пожаловать в CLI для запуска тестов."
+                                    "Что вы хотите сделать?",
+                                    choices=[
+                                        "Заупстить все тесты"
+                                        "Запустить smoke тесты"
+                                        "Запустить regression тесты",
+                                        "Выход"
+                                    ]).ask()
+
+        if choise == "Запустить все тесты":
+            run_pytest(["-v"])
+        elif choice == "Запустить smoke тесты":
+            run_pytest(["-v", "-m", "smoke"])
+        elif choice == "Запустить regression тесты":
+            run_pytest(["-v", "-m", "regression"])
+        elif choice == "Выход":
+            break
+
+if __name__ == "__main__":
+
--- stellar-burgers-main/stellar-burgers-main/src/services/slices/ingredients/actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-
-import { getIngredientsApi } from '../../../utils/burger-api';
-
-export const getIngredientsThunk = createAsyncThunk(
-  'ingredients/getIngredients',
-  async () => getIngredientsApi()
-);
